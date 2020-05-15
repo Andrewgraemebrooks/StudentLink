ee6fabe9dc78c080c9f4cb96b82d64b3c8e7dd4c
Andrew <andrewgraemebrooks@protonmail.com>
2020-05-15 17:27:25 +0200

    Added comments to describe each field of the models so far.
    Added comments to describe each field of the models so far.

--- models/Post.js
+++ models/Post.js
@@ -1,24 +1,31 @@
+// Bring in the mongoose module and the Schema class
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
 const PostSchema = new Schema({
+  // User Id
   user: {
     type: Schema.Types.ObjectId,
     ref: 'users',
   },
+  // Text of the post
   text: {
     type: String,
     required: true,
   },
+  // Name of user
   name: {
     type: String,
   },
+  // Avatar of user
   avatar: {
     type: String,
   },
+  // The likes of the user
   likes: [
     {
+      // User id
       user: {
         type: Schema.Types.ObjectId,
         ref: 'users',
@@ -27,30 +34,37 @@ const PostSchema = new Schema({
   ],
   comments: [
     {
+      // User id
       user: {
         type: Schema.Types.ObjectId,
         ref: 'users',
       },
+      // Text of the comment
       text: {
         type: String,
         required: true,
       },
+      // Name of commenter (seperate so that if the account is deleted, the comment remains)
       name: {
         type: String,
       },
+      // Avatar of commenter (seperate so that if the account is deleted, the comment remains)
       avatar: {
         type: String,
       },
-      data: {
+      // Date of the comment
+      date: {
         type: Date,
         default: Date.now,
       },
     },
   ],
-  data: {
+  // Date of the post
+  date: {
     type: Date,
     default: Date.now,
   },
 });
 
+// Export the post model
 module.exports = Post = mongoose.model('post', PostSchema);
--- models/Profile.js
+++ models/Profile.js
@@ -1,31 +1,39 @@
+// Bring in the mongoose module and the Schema class
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
+  // References the user id
   user: {
     type: Schema.Types.ObjectId,
     ref: 'users',
   },
+  // The handle for the user (a username)
   handle: {
     type: String,
     required: true,
     max: 40,
   },
+  // Which university they go to
   university: {
     type: String,
   },
+  // Which groups they belong to
   groups: {
     type: [String],
     required: true,
   },
+  // There bio
   bio: {
     type: String,
   },
+  // The date that their account was created
   date: {
     type: Date,
     default: Date.now,
   },
 });
 
+// Export the profile model
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
