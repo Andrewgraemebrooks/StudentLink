eb1a274c4e80bc65530f70777bcac8148f638b0b
Andrew Brooks <andrewgraemebrooks@protonmail.com>
2020-06-30 12:07:02 +0200

    Made the footer have a dark background and white text.
    Made the footer have a dark background and white text.

--- front-end/src/components/layout/Footer.js
+++ front-end/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='footer-copyright text-center py-3'>
+    <div className='bg-dark text-white footer-copyright text-center py-3'>
       Copyright © {new Date().getFullYear()} StudentLink
     </div>
   );
