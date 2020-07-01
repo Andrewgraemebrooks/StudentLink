12e175f0dfca5632df59ded2510af17fa61745e2
Andrew Brooks <andrewgraemebrooks@protonmail.com>
2020-07-01 11:05:08 +0200

    Added the to new routes for the login and register components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import NavigationBar from './components/layout/NavigationBar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,10 @@ function App() {
       <div className='App'>
         <NavigationBar />
         <Route exact path='/' component={Landing} />
+        <div className='container'>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </div>
         <Footer />
       </div>
     </Router>
