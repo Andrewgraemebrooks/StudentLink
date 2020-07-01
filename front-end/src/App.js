e78e47af903382fd501ac21cf583d03a22b6322d
Andrew Brooks <andrewgraemebrooks@protonmail.com>
2020-07-01 10:33:05 +0200

    Installed React Router and wrapped current react app.js in the new router.
    Installed React Router and wrapped current react app.js in the new router.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import NavigationBar from './components/layout/NavigationBar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom/BrowserRouter';
 
 function App() {
   return (
-    <div className='App'>
-      <NavigationBar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <NavigationBar />
+        <Route exact path='/' component={Landing} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
