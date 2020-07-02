3b4a4a095b4fe70320582a24f8f2cea458bd579e
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-02 14:13:04 +0200

    Finished the login page

--- front-end/src/components/auth/Login.js
+++ front-end/src/components/auth/Login.js
@@ -2,9 +2,54 @@ import React, { Component } from 'react';
 
 class Login extends Component {
   render() {
-    return <div>
-        <h1>Login</h1>
-    </div>;
+    return (
+      <div>
+        <div className="container">
+          <div className="login">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">
+                  Login into StudentLink
+                </h1>
+                <form>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md12">
+                      <div className="form-group">
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                          className="form-control input-sm"
+                          placeholder="Email Address"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12">
+                      <div className="form-group">
+                        <input
+                          type="password"
+                          name="password"
+                          id="password"
+                          className="form-control input-sm"
+                          placeholder="Password"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <input
+                    type="submit"
+                    value="Login"
+                    className="btn btn-info btn-block btn-danger"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Login;
