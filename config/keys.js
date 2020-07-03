74c8fc72c85360fbcab54fd51a86268bfa2ab00a
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-03 21:00:02 +0200

    Updated mongodb to work from a localhost because I had issues with the cloud server.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://admin:L7QfDFfeCOYJJaYS@cluster0-cnlxu.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  // mongoURI: 'mongodb+srv://admin:L7QfDFfeCOYJJaYS@cluster0-cnlxu.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  mongoURI: 'mongodb://localhost:27017/finalyearproject',
   secretOrKey: 'secret',
 };
