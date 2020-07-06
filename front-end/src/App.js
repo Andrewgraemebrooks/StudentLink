2847431b810f2bcebbc13c10eaa178536ee8ef28
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-06 11:18:43 +0200

    Installed redux and created a basic redux setup for App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,20 +5,26 @@ import Footer from './components/layout/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <NavigationBar />
-        <Route exact path='/' component={Landing} />
-        <div className='container'>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <NavigationBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
