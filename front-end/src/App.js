3021a428112d88067232bc18e0d5b519339e0c12
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-06 11:22:15 +0200

    Moved redux store logic from the main app.js file to it's own seperate file.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,9 +6,6 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-
-const store = createStore(() => [], {}, applyMiddleware())
 
 function App() {
   return (
