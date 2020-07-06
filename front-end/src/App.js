99f8bf179d42d8bb6efe0744c4d8d5a65c4d1a6f
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-06 12:59:25 +0200

    Finished setting up redux with a store and middleware, the extention now recognises the redux componenets

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
