6f67aab61bd7b30c96481703b06bf0cb854ab903
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-08 16:09:41 +0200

    The user can now log in!
    The user can log and their jwttoken is stored in state which means that they stay logged in as long as the token is valid. The token is passed from state to every api call for private routes.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,19 @@ import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 function App() {
   return (
--- front-end/src/components/auth/Login.js
+++ front-end/src/components/auth/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
+import classnames from 'classnames';
 
 class Login extends Component {
   constructor() {
@@ -13,6 +17,16 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/profile');
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -20,59 +34,78 @@ class Login extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
+    const userData = {
       email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
     };
 
-    console.log(user);
+    this.props.loginUser(userData);
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div>
-        <div className='container'>
-          <div className='login'>
-            <div className='row'>
-              <div className='col-md-8 m-auto'>
-                <h1 className='display-4 text-center'>
+        <div className="container">
+          <div className="login">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">
                   Login into StudentLink
                 </h1>
                 <form onSubmit={this.onSubmit}>
-                  <div className='row'>
-                    <div className='col-xs-12 col-sm-12 col-md12'>
-                      <div className='form-group'>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md12">
+                      <div className="form-group">
                         <input
-                          type='email'
-                          name='email'
-                          id='email'
-                          className='form-control input-sm'
-                          placeholder='Email Address'
+                          type="email"
+                          name="email"
+                          id="email"
+                          className={classnames('form-control input-sm', {
+                            // is-valid will only exist if errors.name exists
+                            'is-invalid': errors.email,
+                          })}
+                          placeholder="Email Address"
                           value={this.state.email}
                           onChange={this.onChange}
                         />
+                        {/* If there is an error, returns the error underneath the input field */}
+                        {errors.email && (
+                          <div className="invalid-feedback">
+                            {errors.email}{' '}
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
-                  <div className='row'>
-                    <div className='col-xs-12 col-sm-12 col-md-12'>
-                      <div className='form-group'>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12">
+                      <div className="form-group">
                         <input
-                          type='password'
-                          name='password'
-                          id='password'
-                          className='form-control input-sm'
-                          placeholder='Password'
+                          type="password"
+                          name="password"
+                          id="password"
+                          className={classnames('form-control input-sm', {
+                            // is-valid will only exist if errors.name exists
+                            'is-invalid': errors.password,
+                          })}
+                          placeholder="Password"
                           value={this.state.password}
                           onChange={this.onChange}
                         />
+                        {/* If there is an error, returns the error underneath the input field */}
+                        {errors.password && (
+                          <div className="invalid-feedback">
+                            {errors.password}{' '}
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
                   <input
-                    type='submit'
-                    value='Login'
-                    className='btn btn-info btn-block btn-danger'
+                    type="submit"
+                    value="Login"
+                    className="btn btn-info btn-block btn-danger"
                   />
                 </form>
               </div>
@@ -83,4 +116,16 @@ class Login extends Component {
     );
   }
 }
-export default Login;
+
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
