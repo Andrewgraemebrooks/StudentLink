149abb73718765dc705a13544cea38db1d1aa73a
Andrew <andrewgraemebrooks@protonmail.com>
2020-07-10 09:53:57 +0200

    The application can successfully call the profile into state and also clear the profile from state when logged out.
    The application can successfully call the profile into state and also clear the profile from state when logged out.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import NavigationBar from './components/layout/NavigationBar';
-import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
+import NavigationBar from './components/common/NavigationBar';
+import Landing from './components/common/Landing';
+import Footer from './components/common/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Profile from './components/profile/Profile';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -19,7 +21,8 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
-
+  // Clear current profile
+  store.dispatch(clearCurrentProfile())
   // Check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
@@ -39,6 +42,7 @@ function App() {
           <div className="container">
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
           </div>
           <Footer />
         </div>
