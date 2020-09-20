836f48bac9a937abd1de0fb0e50b3b30d4f9ae50
Andrew <andrewgraemebrooks@protonmail.com>
2020-09-20 16:21:42 +0200

    Installed yarn.locks and updated config
    Included a yarn.lock to make sure that when developers fork the repository that the dependencies won't break due to versioning issues. I also got rid of an old config comment for mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // mongoURI: 'mongodb+srv://admin:L7QfDFfeCOYJJaYS@cluster0-cnlxu.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  mongoURI: 'mongodb://localhost:27017/finalyearproject',
-  secretOrKey: 'secret',
+  mongoURI: "mongodb://localhost:27017/studentlink",
+  secretOrKey: "secret",
 };
