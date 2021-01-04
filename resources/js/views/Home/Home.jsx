85af2045bf2f096b2628197bf8d3a117da59b972
Andrew <andrewgraemebrooks@protonmail.com>
2021-01-04 16:02:06 +0100

    Created Home Page

--- resources/js/views/Home/Home.jsx
+++ resources/js/views/Home/Home.jsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Home(props) {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+      email: '',
+      password: '',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onLoginSubmit = this.onLoginSubmit.bind(this);
+    this.onSignupSubmit = this.onSignupSubmit.bind(this);
+  }
+
+  /**
+   * Updates the state when the value of an element has been changed.
+   * @param {React.ChangeEvent<HTMLInputElement>} event
+   */
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  /**
+   * Logs the user in.
+   * @param {React.MouseEvent<HTMLButtonElement, MouseEvent>} event
+   */
+  onLoginSubmit(event) {
+    event.preventDefault();
+  }
+
+  /**
+   * Sends the user to the register page.
+   * @param {React.MouseEvent<HTMLButtonElement, MouseEvent>} event
+   */
+  onSignupSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Hello</h1>
+      <div id="home-container" className="container">
+        <div className="row">
+          <div className="col text-col">
+            <h1>StudentLink</h1>
+            <h6>Connect with other students!</h6>
+          </div>
+          <div className="col">
+            <form>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  onChange={this.onChange}
+                  placeholder="Enter email"
+                />
+              </div>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                onChange={this.onChange}
+                placeholder="Enter password"
+              />
+              <button className="btn btn-primary" onClick={this.onLoginSubmit}>
+                Log In
+              </button>
+              <hr />
+              <button className="btn btn-success" onClick={this.onSignupSubmit}>
+                Create A New Account
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
     );
+  }
 }
 
 export default Home;
