f551c54395f59b35a25be0a0fa45df8e00e77aa9
Andrew <andrewgraemebrooks@protonmail.com>
2021-01-07 14:23:16 +0100

    Created a sidebar component
    Problem(s):
    * We did not have a reusable sidebar component.
    
    Solution(s):
    * Created a react component that creates a sidebar with items that are passed to it as props

--- resources/js/views/Home/Home.jsx
+++ resources/js/views/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 // Like Twitter's Home/Newsfeed
+
+const sidebarItems = [
+  { name: 'home', label: 'Home' },
+  { name: 'profile', label: 'Profile' },
+  { name: 'notifications', label: 'Notifications' },
+  { name: 'messages', label: 'Messages' },
+  { name: 'settings', label: 'Settings' },
+];
 
 function Home(props) {
   return (
     <div>
-      <h1>Hello</h1>
+      <Sidebar items={sidebarItems} />
     </div>
   );
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Eventer</title>
+    <title>StudentLink</title>
     <link rel="icon" type="image/png" href="media/Logo/favicon-32x32.png" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
