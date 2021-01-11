9bdcfe78ee495ad330ffdd35fdd1bc2acb09f546
Andrew Brooks <andrewgraemebrooks@protonmail.com>
2021-01-11 11:11:27 +0100

    Fixed a line in the web.php file to allow React Router to work and Installed Telescope

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * The line below redirects all first level url paths in the browser to index view,
+ * this implies that paths like localhost:8080/test or localhost:8080/error will
+ * redirect us to the index view while higher orders like localhost:8080/test/error
+ * or localhost:8080/test/error/sad will return Laravel’s default 404 page
+ */
+Route::view('/{path?}', 'welcome');
